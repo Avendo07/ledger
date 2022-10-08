7ec226dc9e5392d306ba853a5cda21ce1d07fc70
Avendo07 <namankaushikplt@gmail.com>
2022-10-08 23:42:07 +0530

    Updated the function for matcher

--- test/trials.dart
+++ test/trials.dart
@@ -9,10 +9,35 @@ void main() {
     });
 
     Map<String, String> matcher(String template, String data) {
-      List templateList = template.split(" ");
-      List dataList = data.split(" ");
+      List<String> tempList = template.split("/@");
+      List<String> dataList = data.split(" ");
       Map<String, String> extractedData = {};
-      String templateVar = "";
+      int sumYet = 0;
+      print(tempList);
+      for (int index = 0; index < tempList.length - 1; index++) {
+        print(dataList);
+        List<String> tempSubList = tempList[index + 1].split(" ");
+        tempSubList.removeWhere((element) => element == "");
+        List<String> prevTempSubList = tempList[index].split(" ");
+        prevTempSubList.removeWhere((element) => element == "");
+        int position = dataList.indexOf(prevTempSubList.last) + 1;
+        // print(tempSubList[1]);
+        int endPosition;
+        if (tempSubList.length == 1) {                                          //Move from here
+          endPosition = dataList.length;
+        } else {
+          endPosition = dataList.indexOf(tempSubList[1]);
+        }
+        print("$position $endPosition $prevTempSubList $index $sumYet");
+        String finalString = dataList
+            .getRange(position, endPosition)
+            .fold("", (value, e) => "$value $e");
+        extractedData[tempSubList[0]] = finalString;
+        sumYet = sumYet + prevTempSubList.length;
+      }
+      return extractedData;
+
+      /*String templateVar = "";                                                //V2
       String buffer = "";
       for (int tempIndex = 0, dataIndex = 0; tempIndex < templateList.length;) {
         if (templateList[tempIndex].startsWith("/@")) {
@@ -41,8 +66,8 @@ void main() {
             print("${templateList[tempIndex]}: ${dataList[dataIndex]}!=\n");
             dataIndex++;
           }
-        }
-        /*if (templateList[tempIndex] == dataList[dataIndex]) {
+        }*/
+      /*if (templateList[tempIndex] == dataList[dataIndex]) {                 V1
           print("${templateList[tempIndex]}: ${dataList[dataIndex]}==\n");
           if(buffer != ""){
             extractedData[templateList[tempIndex]] = buffer;
@@ -56,13 +81,13 @@ void main() {
           print("${templateList[tempIndex]}: ${dataList[dataIndex]}!=\n");
           dataIndex++;
         }*/
-      }
+
       return extractedData;
     }
 
     test("diff_match_patch 1 ", () {
-      String mainString = "Hello Naman spent Rs 100 on 21 August";
-      String template = "Hello /@name spent Rs /@amount on /@date";
+      String mainString = "Hello Naman spent Rs 100 on 21 August here";
+      String template = "Hello /@name spent Rs /@amount on /@date here";
       Map<String, String> differences = matcher(template, mainString);
       //List<Diff> differences = diff(template, mainString);
       print(differences);
@@ -77,9 +102,10 @@ void main() {
     });
 
     test("diff_match_patch 3", () {
-      String mainString = "Hello Naman spent Rs 100 on 21 August at 10 AM time";
+      String mainString =
+          "Hello Naman spent Rs 100 on 21 August at 10 AM time in Delhi place";
       String template =
-          "Hello /@name spent Rs /@amount on /@date at /@time time";
+          "Hello /@name spent Rs /@amount on /@date at /@time time in /@place place";
       Map<String, String> differences = matcher(template, mainString);
       //List<Diff> differences = diff(template, mainString);
       print(differences);
@@ -100,6 +126,26 @@ void main() {
       Map<String, String> differences = matcher(template, mainString);
       //List<Diff> differences = diff(template, mainString);
       print(differences);
+    });
+
+    test("diff_match_pathch 6", () {
+      String mainString = "Hello Naman 100 on 21 at August at 10 AM tim";
+      String template = "Hello /@name /@amount on /@date at /@time at tim";
+      Map<String, String> differences = matcher(template, mainString);
+      //List<Diff> differences = diff(template, mainString);
+      print(differences);
+    });
+
+    test("reg ex", () {
+      // RegExp reg = RegExp(r'Hello * how are you');
+      // print(reg.hasMatch('Hello naman how are you'));
+      // print(reg);
+
+      var string = 'Humming is a bird';
+      var regExp = RegExp(r'Humming .* bird');
+      var match = regExp.hasMatch(string);
+      // regExp.stringMatch(input)
+      print(match);
     });
   });
 }
